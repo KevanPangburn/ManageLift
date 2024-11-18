933352f5499c5d11843cb1db5a04213798b04da5
Kevan Pangburn <kevan.pangburn@gmail.ocm>
2024-11-18 10:55:11 -0500

    Initial commit: Add project files

---
+++ src/main/java/com/ManageLift/forklift_management/ManageLiftApplication.java
@@ -0,0 +1,13 @@
+package com.ManageLift.forklift_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageLiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageLiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ManageLift/forklift_management/ForkliftManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ManageLift.forklift_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkliftManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
