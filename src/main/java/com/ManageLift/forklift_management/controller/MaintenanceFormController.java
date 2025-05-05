6a5d2b84b67031949ab079f6000887e42de4dde7
Kevan Pangburn <kevan.pangburn@gmail.ocm>
2025-05-05 13:33:30 -0400

    Form started

--- src/main/java/com/ManageLift/forklift_management/controller/MaintenanceFormController.java
+++ src/main/java/com/ManageLift/forklift_management/controller/MaintenanceFormController.java
@@ -1,4 +1,13 @@
 package com.ManageLift.forklift_management.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MaintenanceFormController {
+
+    @GetMapping("/maintenance_form")
+    public String showMaintenanceForm() {
+        return "maintenance_form";
+    }
 }
